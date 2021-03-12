f454b9c9c0fd39d9470bac1569d244818c2b7bbd
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-11 18:05:05 -0800

    lint errors

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation} = require('./iss');
+const {nextISSTimesForMyLocation} = require('./iss');
 
-/*
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned IP:' , ip);
-});
-
-fetchCoordsByIP('173.180.118.155', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned coordinates:' , coordinates);
-});
-
-
-fetchISSFlyOverTimes({ latitude: 49.2732, longitude: -123.0124 }, (error, response) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned coordinates:' , response);
-});
-
-*/
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
